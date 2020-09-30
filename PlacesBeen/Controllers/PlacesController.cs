ce04d83b3c69406244a0c9e94d8dce74eaf358e6
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-30 16:38:31 -0700

    Add additional categories to city details page.
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- PlacesBeen/Controllers/PlacesController.cs
+++ PlacesBeen/Controllers/PlacesController.cs
@@ -7,25 +7,41 @@ namespace PlacesBeen.Controllers
   public class PlacesController : Controller 
   {
     [HttpGet("/places")]
-  public ActionResult Index() 
+  public ActionResult Index()               //Common RESTful route name when you want to display list of all objects (GET)
   {
     List<Place> allPlaces = Place.GetAll();
     return View(allPlaces);
   } 
   [HttpPost("/places")]
-   public ActionResult Create(string cityName) 
+   public ActionResult Create(string cityName, string days, string travelBuddy, string dishes)   //Creates a new item on the server (POST)
   {
-    Place myPlace = new Place(cityName);
+    int daysStayed = int.Parse(days);
+    Place myPlace = new Place(cityName, daysStayed, travelBuddy, dishes);
     return RedirectToAction("Index");
   }
 
   [HttpGet("/places/new")]
-  public ActionResult New()
+  public ActionResult New()           //Offers form to create new object (GET)
   {
     return View();
   }    
 
-
-    }
+  [HttpGet("/places/{id}")]
+  public ActionResult Show(int id)
+  {
+    Place foundPlace = Place.Find(id);   //Create .Find() method in class
+    return View(foundPlace);
   }
+
+  [HttpPost("/places/{id}")]
+  public ActionResult Update(int id)
+  {
+    Place foundPlace = Place.Find(id);
+    foundPlace.VisitAgain = true;
+    return RedirectToAction("Index");
+  }
+
+
+  }
+}
 
--- PlacesBeen/Models/Place.cs
+++ PlacesBeen/Models/Place.cs
@@ -3,20 +3,42 @@ using System;
 
 namespace PlacesBeen.Models
 {
-  public class Place
+  public class Place                        // Create class
   {
-    public string CityName { get; set; }
-    private static List<Place> _instances = new List<Place> { };
+    public string CityName { get; set; }            //Give it the property cityName (#2)
+    public int Id { get; }
+    private static List<Place> _instances = new List<Place> { }; //For testing: public static List<Place> GetPlaces = new List<Place> { };
+    public int DaysStayed { get; set; }
+    public string TravelBuddy { get; set; }
 
-    public Place(string cityName)
+    public string Dishes {get; set;}
+    public bool VisitAgain { get; set; }
+
+
+    public Place(string cityName, int daysStayed, string travelBuddy, string dishes)         // Create Place object (#1)
     {
       CityName = cityName;
+      DaysStayed = daysStayed;
+      TravelBuddy = travelBuddy;
+      Dishes = dishes;
+      VisitAgain = false;
       _instances.Add(this);
+      Id = _instances.Count;
     }
-    public static List<Place> GetAll()
+    public static List<Place> GetAll()   // Method that will return the full list of that particular instance
     {
       return _instances;
-      
+    }
+
+    public static Place Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
+
+//Places I've Visited: Portland   Places I Want To Visit Again: (blank)
+//Do you want to visit again?: false;
+//Portland Details: All the details and (They'll be a button that says "Visit Again?)
+//"Visit Again" button will turn VisitAgain = true;
+//Places I Want To Visit Again: Portland
